de13a9c439118eda0204b31e0632866da8cf882e
sajan <sajankhad2@gmail.com>
2023-06-09 15:05:13 +0545

    ajax try

--- mainapp/views.py
+++ mainapp/views.py
@@ -9,6 +9,9 @@ from .forms import CSVUploadForm
 
 
 
+
+
+
 def auto_download(request):
     if request.method == 'POST':
         company = request.POST.get('company')
@@ -72,101 +75,106 @@ def pred_show(request):
 
 def predict(request):
     if request.method == 'POST' and request.FILES['csv_file']:
-        import pandas as pd
-        import numpy as np
-        from sklearn.model_selection import train_test_split
-        from sklearn.preprocessing import StandardScaler
-        from sklearn.metrics import mean_squared_error, r2_score
-        from tensorflow.keras.models import Sequential
-        from tensorflow.keras.layers import LSTM, Dense
-
+        from .lstm import lstm_model
         csv_file = request.FILES['csv_file']
+        data = lstm_model(csv_file)
+        return render(request, 'done.html', {'data':data})
+    return render(request, 'predict.html')
+        # import pandas as pd
+        # import numpy as np
+        # from sklearn.model_selection import train_test_split
+        # from sklearn.preprocessing import StandardScaler
+        # from sklearn.metrics import mean_squared_error, r2_score
+        # from tensorflow.keras.models import Sequential
+        # from tensorflow.keras.layers import LSTM, Dense
 
-        # Load the data from the CSV file
-        data = pd.read_csv(csv_file)
+        # csv_file = request.FILES['csv_file']
 
-        # Remove the 'Symbol' attribute
-        data = data.drop('Symbol', axis=1)
+        # # Load the data from the CSV file
+        # data = pd.read_csv(csv_file)
 
-        # Convert 'Date' column to datetime format
-        data['Date'] = pd.to_datetime(data['Date'])
+        # # Remove the 'Symbol' attribute
+        # data = data.drop('Symbol', axis=1)
 
-        # Preprocess the data
-        X = data.drop(['Close', 'Date'], axis=1)
-        y = data['Close']
+        # # Convert 'Date' column to datetime format
+        # data['Date'] = pd.to_datetime(data['Date'])
 
-        # Split the data into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        # # Preprocess the data
+        # X = data.drop(['Close', 'Date'], axis=1)
+        # y = data['Close']
 
-        # Scale the features
-        scaler = StandardScaler()
-        X_train_scaled = scaler.fit_transform(X_train)
-        X_test_scaled = scaler.transform(X_test)
+        # # Split the data into training and testing sets
+        # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        # Reshape the input data for LSTM
-        X_train_lstm = X_train_scaled.reshape((X_train_scaled.shape[0], 1, X_train_scaled.shape[1]))
-        X_test_lstm = X_test_scaled.reshape((X_test_scaled.shape[0], 1, X_test_scaled.shape[1]))
+        # # Scale the features
+        # scaler = StandardScaler()
+        # X_train_scaled = scaler.fit_transform(X_train)
+        # X_test_scaled = scaler.transform(X_test)
 
-        # Build the LSTM model
-        model = Sequential()
-        model.add(LSTM(50, activation='relu', input_shape=(X_train_lstm.shape[1], X_train_lstm.shape[2])))
-        model.add(Dense(1))
-        model.compile(optimizer='adam', loss='mse')
+        # # Reshape the input data for LSTM
+        # X_train_lstm = X_train_scaled.reshape((X_train_scaled.shape[0], 1, X_train_scaled.shape[1]))
+        # X_test_lstm = X_test_scaled.reshape((X_test_scaled.shape[0], 1, X_test_scaled.shape[1]))
 
-        # Train the LSTM model
-        model.fit(X_train_lstm, y_train, epochs=50, batch_size=32, verbose=0)
+        # # Build the LSTM model
+        # model = Sequential()
+        # model.add(LSTM(50, activation='relu', input_shape=(X_train_lstm.shape[1], X_train_lstm.shape[2])))
+        # model.add(Dense(1))
+        # model.compile(optimizer='adam', loss='mse')
 
-        # Ask the user for a date to predict the close price
-        user_date = input("Enter the date (in the format YYYY-MM-DD) for which you want to predict the close price: ")
+        # # Train the LSTM model
+        # model.fit(X_train_lstm, y_train, epochs=50, batch_size=32, verbose=0)
 
-        # Convert the user date to datetime
-        user_date = pd.to_datetime(user_date)
+        # # Ask the user for a date to predict the close price
+        # user_date = input("Enter the date (in the format YYYY-MM-DD) for which you want to predict the close price: ")
 
-        # Find the row index corresponding to the user date
-        user_date_index = data[data['Date'] == user_date].index[0]
+        # # Convert the user date to datetime
+        # user_date = pd.to_datetime(user_date)
 
-        # Get the attributes for the user date
-        user_data = data.drop(['Close', 'Date'], axis=1).loc[user_date_index].values.reshape(1, -1)
+        # # Find the row index corresponding to the user date
+        # user_date_index = data[data['Date'] == user_date].index[0]
 
-        # Scale the user data
-        user_data_scaled = scaler.transform(user_data)
+        # # Get the attributes for the user date
+        # user_data = data.drop(['Close', 'Date'], axis=1).loc[user_date_index].values.reshape(1, -1)
 
-        # Reshape the user data for LSTM
-        user_data_lstm = user_data_scaled.reshape((user_data_scaled.shape[0], 1, user_data_scaled.shape[1]))
+        # # Scale the user data
+        # user_data_scaled = scaler.transform(user_data)
 
-        # Predict the close price for the user date
-        predicted_close_price = model.predict(user_data_lstm)
+        # # Reshape the user data for LSTM
+        # user_data_lstm = user_data_scaled.reshape((user_data_scaled.shape[0], 1, user_data_scaled.shape[1]))
 
-        print("Predicted Close Price:", predicted_close_price[0][0])
+        # # Predict the close price for the user date
+        # predicted_close_price = model.predict(user_data_lstm)
 
-        # Predict on the test set
-        y_pred_lstm = model.predict(X_test_lstm)
+        # print("Predicted Close Price:", predicted_close_price[0][0])
 
-        # Reshape y_pred_lstm
-        y_pred_lstm = y_pred_lstm.reshape((y_pred_lstm.shape[0],))
+        # # Predict on the test set
+        # y_pred_lstm = model.predict(X_test_lstm)
 
-        # Inverse scale the predictions
-        y_pred_lstm = scaler.inverse_transform(X_test_scaled)[:, 0]
+        # # Reshape y_pred_lstm
+        # y_pred_lstm = y_pred_lstm.reshape((y_pred_lstm.shape[0],))
 
-        # Calculate RMSE
-        rmse_lstm = np.sqrt(mean_squared_error(y_test, y_pred_lstm))
-        print("Root Mean Squared Error (RMSE):", rmse_lstm)
+        # # Inverse scale the predictions
+        # y_pred_lstm = scaler.inverse_transform(X_test_scaled)[:, 0]
 
-        # Calculate MSE
-        mse_lstm = mean_squared_error(y_test, y_pred_lstm)
-        print("Mean Squared Error (MSE):", mse_lstm)
+        # # Calculate RMSE
+        # rmse_lstm = np.sqrt(mean_squared_error(y_test, y_pred_lstm))
+        # print("Root Mean Squared Error (RMSE):", rmse_lstm)
 
-        # Calculate R2 score
-        r2_lstm = r2_score(y_test, y_pred_lstm)
-        print("R2 Score:", r2_lstm)
+        # # Calculate MSE
+        # mse_lstm = mean_squared_error(y_test, y_pred_lstm)
+        # print("Mean Squared Error (MSE):", mse_lstm)
 
-        data = {
-            'close_price': predicted_close_price,
-            'rmse': rmse_lstm,
-            'mse': mse_lstm,
-            'r2': r2_lstm
-        }
-        return render(request, 'done.html', data)
+        # # Calculate R2 score
+        # r2_lstm = r2_score(y_test, y_pred_lstm)
+        # print("R2 Score:", r2_lstm)
+
+        # data = {
+        #     'close_price': predicted_close_price,
+        #     'rmse': rmse_lstm,
+        #     'mse': mse_lstm,
+        #     'r2': r2_lstm
+        # }
+        # return render(request, 'done.html', data)
 
 
 
